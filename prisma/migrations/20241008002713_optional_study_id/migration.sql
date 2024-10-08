668e61504973ef8fd370df8c9e5c5426489a95b2
alscksdlek <mseon916@naver.com>
2024-10-08 09:45:54 +0900

    feat: studyId optional field 설정

---
+++ prisma/migrations/20241008002713_optional_study_id/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "CompletedHabit" ALTER COLUMN "studyId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Habit" ALTER COLUMN "studyId" DROP NOT NULL;
--- utils/habitStruct.js
+++ utils/habitStruct.js
@@ -2,5 +2,5 @@ import * as s from "superstruct";
 
 export const CreateHabit = s.object({
   habitName: s.string(),
-  studyId: s.string(),
+  studyId: s.optional(s.string()),
 });
