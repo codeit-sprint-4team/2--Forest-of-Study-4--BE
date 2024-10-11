d19891915c2d98364a5a235f2400ec59ce4ec431
alscksdlek <mseon916@naver.com>
2024-10-11 19:41:07 +0900

    chore: 프로젝트 기초 셋팅 재작업

---
+++ prisma/seed/index.js
@@ -0,0 +1,16 @@
+//시딩
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  // 각 시딩 파일에서 시딩 함수 호출
+}
+//데이터베이스와의 연결 종료
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
