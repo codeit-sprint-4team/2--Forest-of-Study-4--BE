2cd7e2ca5e22d9894e5c47724987abdd4035849c
rlackgus <999snowman@naver.com>
2024-10-12 16:33:04 +0900

    feat:스터디 api 구현 및 스키마 필드추가

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 dotenv.config();
 import { router as habitRoutes } from "./routes/habitRoutes.js";
+import studyRoutes from "./routes/studyRoutes.js";
 
 const corsOptions = {
   origin: "*",
@@ -13,5 +14,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/habits", habitRoutes);
+app.use("/api", studyRoutes);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
